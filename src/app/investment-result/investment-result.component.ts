6f004edbd8f27c195d3944744fe98d467fe08e46
hariprasad <myharicode@gmail.com>
2024-06-09 01:24:25 -0300

    first commit

---
+++ src/app/investment-result/investment-result.component.ts
@@ -0,0 +1,16 @@
+import { CurrencyPipe } from '@angular/common';
+import { Component, computed, inject } from '@angular/core';
+import { InvestmentService } from '../investment.service';
+
+@Component({
+  selector: 'app-investment-result',
+  standalone: true,
+  imports: [CurrencyPipe],
+  templateUrl: './investment-result.component.html',
+  styleUrl: './investment-result.component.css',
+})
+export class InvestmentResultComponent {
+  private investmentService = inject(InvestmentService);
+
+  results = this.investmentService.resultData.asReadonly();
+}
